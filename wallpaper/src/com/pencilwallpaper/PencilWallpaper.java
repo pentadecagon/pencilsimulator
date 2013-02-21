ad001d9f4c4c794bd635a1fe53310b42b0b8e6e4
lcrudd <lcrudd@googlemail.com>
2013-02-21 12:58:29 +0000

    increased gravity to 0.02

--- wallpaper/src/com/pencilwallpaper/PencilWallpaper.java
+++ wallpaper/src/com/pencilwallpaper/PencilWallpaper.java
@@ -52,7 +52,7 @@ public class PencilWallpaper extends WallpaperService {
     	
     	//artificial parameter we use to reduce the size of gravity for testing because pencil falls over too fast
     	//with real gravity
-    	final private double GRAVITY_REDUCTION_FACTOR = 0.01;
+    	final private double GRAVITY_REDUCTION_FACTOR = 0.02;
     	
     	//angle of gravitational force from negative y axis
     	private double theta = 0.0;
